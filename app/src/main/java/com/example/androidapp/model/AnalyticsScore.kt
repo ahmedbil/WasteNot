d6ca2136ef075f3767620133330bb9ba2c89b2e4
Snehit Khair <darkfire6601@gmail.com>
2023-07-26 13:31:21 -0400

    modded wiring for Str to int in endpoint call

--- app/src/main/java/com/example/androidapp/model/AnalyticsScore.kt
+++ app/src/main/java/com/example/androidapp/model/AnalyticsScore.kt
@@ -2,7 +2,7 @@
 enum class AnalyticsScore(val numericalScore: Int) {
     ENTIRELY_WASTEFUL(1), // 80%+ of inventory wasted
     MOSTLY_WASTEFUL(2), // 60%-79% of inventory wasted
-    OVER_CONSUMER(3), // 30%-69%  of inventory wasted
+    OVER_CONSUMER(3), // 30%-59%  of inventory wasted
     STANDARD_CONSUMER(4), // 15%-29%  of inventory wasted
     MINDFUL_CONSUMER(5), // 5%-14%  of inventory wasted
     ENTIRELY_SUSTAINABLE(6), // 0%-4%  of inventory wasted
